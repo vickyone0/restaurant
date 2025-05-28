d69c1c447293001aa8b336c0c9e8bd8dd131b986
vickyone0 <vigneshs0.618@gmail.com>
2025-05-28 05:22:36 -0400

    second commit

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,6 @@ mod front_of_house {
         }
     }
 
-    
-
     mod serving {
         fn take_order() {
             // Function implementation
@@ -24,12 +22,51 @@ mod front_of_house {
     }
 }
 
-
 pub fn eat_at_restaurant() {
-        
-        //Absoult path
-        crate::front_of_house::hosting::add_to_waitlist();
 
-        // Relative path
-        front_of_house::hosting::add_to_waitlist();
-    } 
+
+ let order1 = back_of_house::Appetizer::Soup;
+ let order2 = back_of_house::Appetizer::Salad;
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like.
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    //Absoult path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+}
+
+fn deliver_order() {}
+mod back_of_house {
+
+    pub enum Appetizer {
+ Soup,
+ Salad,
+ }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+    fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+
+        pub fn seasonal_fruit(&self) -> &str {
+            &self.seasonal_fruit
+        }
+    }
+}
